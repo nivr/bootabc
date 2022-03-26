db18769b5c7e6d53d509f6e7aa6a10026298fc92
NvR <n.vanryn@gmail.com>
2022-03-26 11:07:14 +0100

    bootstrap_measures()
    First working version, not crazy fast

--- R/bootstrap_measures.R
+++ R/bootstrap_measures.R
@@ -1,12 +1,13 @@
 #' Bootstrap customer data from an A/B-test
 #'
-#' @param input_data A grouped data frame with one row per customer/visitor. It
-#' should contain a (grouped-by) column with the test variant and additional
-#' columns for each measure in the test for that customer/visitor.
+#' @param input_data_frame A grouped data frame with one row per
+#' customer/visitor. It should contain a (grouped-by) column with the test
+#' variant and additional columns for each measure in the test for that
+#' customer/visitor.
 #' @param bootstrap_iterations The number of bootstrap iterations to calculate,
 #' the default is 100.
-#' @param ... <[`data-masking`][dplyr::dplyr_data_masking]> Name-value pairs of summary
-#'   functions. The name will be the name of the variable in the result.
+#' @param ... <[`data-masking`][dplyr::dplyr_data_masking]> Name-value pairs of
+#' summary functions. The name will be the name of the variable in the result.
 #'
 #'   The value can be:
 #'
@@ -19,9 +20,85 @@
 #' @export
 #'
 #' @examples
-#' @importFrom dplyr summarise
-bootstrap_measures <- function(input_data,
+#' example_data <- data.frame(
+#'                 x = rep(stats::runif(10),2),
+#'                 group = c("A", "B")
+#'                 ) %>%
+#'                 group_by(group)
+#'bootstrap_measures(example_data, kpi1=mean(x))
+#' @importFrom dplyr summarise group_vars group_keys count across
+#' @importFrom dplyr all_of bind_rows
+#' @importFrom data.table as.data.table data.table rbindlist
+#' @importFrom data.table merge.data.table :=
+#' @importFrom rlang enexprs
+#' @importFrom dqrng dqsample.int
+#' @importFrom magrittr %>%
+bootstrap_measures <- function(input_data_frame,
                                bootstrap_iterations = 100,
                                ...) {
-  dplyr::sample_n(input_data,bootstrap_iterations,replace = TRUE)
+  bootstrap_iteration <- NULL
+
+  if (!is.grouped_df(input_data_frame)) {
+    stop("Your input data must be a grouped data.frame")
+  }
+
+  bootstrap_results <- data.frame()
+  data_table <- data.table::as.data.table(input_data_frame)
+
+  group_column <- dplyr::group_vars(input_data_frame)
+  group_names <- dplyr::group_keys(input_data_frame)
+  group_filter <- group_names %>%
+    sapply(function(.x) {
+      paste0(group_column, " == \"", .x, "\"")
+    })
+  group_sizes <- dplyr::count(input_data_frame,
+                              dplyr::across(dplyr::all_of(group_column)))$n
+  for (group in seq_along(group_sizes)) {
+    evaluation_expression <- .get_evaluation_string(.get_expressions(...),
+                                                group_column,
+                                                group_names,
+                                                group)
+    data_table_group <- data_table[eval(parse(text = group_filter[[group]]))]
+    i <- 0
+    group_result <- data.table::merge.data.table(
+      data.table::data.table(bootstrap_iteration = rep(NA_real_,
+                                                       bootstrap_iterations)),
+      data_table[0, eval(evaluation_expression)],
+      all.x = TRUE
+    )
+
+    for (i in seq(bootstrap_iterations)) {
+      bootstrap_shuffle <- dqrng::dqsample.int(group_sizes[[group]],
+                                               replace = TRUE)
+      group_result[i, ] <-
+        data_table_group[bootstrap_shuffle,
+                        eval(evaluation_expression)][, bootstrap_iteration := i]
+    }
+    bootstrap_results <- dplyr::bind_rows(bootstrap_results,
+                                   group_result)
+
+  }
+
+bootstrap_results
+}
+
+.get_expressions <- function(...) {
+  rlang::enexprs(..., .ignore_empty = "all")
+}
+
+.get_evaluation_string <- function(evaluation_expressions,
+                                   group_column,
+                                   group_names,
+                                   group) {
+  parse(
+    text = paste0(
+      ".(",
+      "bootstrap_iteration = i, ",
+      paste0(group_column, " = ", "\"", group_names[[group, 1]], "\", "),
+      paste(paste0(names(evaluation_expressions),
+                   " = ",
+                   evaluation_expressions), collapse = ", "),
+      ")"
+    )
+  )
 }
--- tests/testthat/test-bootstrap_measures.R
+++ tests/testthat/test-bootstrap_measures.R
@@ -1,25 +1,43 @@
-test_that("Output is a data frame", {
+test_that("Expect error if input is an ungrouped data frame", {
   example_data <- data.frame(
-    x = c(
-      stats::runif(10),
-      stats::runif(10)
-    ),
-    group = c("A", "B")
-  )
-  expect_s3_class(bootstrap_measures(example_data), "data.frame")
+    measure = 1:10,
+    group = "A"
+    )
+
+  expect_error(bootstrap_measures(example_data,
+                                     kpi1 = mean(measure)
+  ), "grouped data.frame")
 })
 
-test_that("Output has the same number of rows as specified in input", {
-  bootstrap_iterations <- 1733
-  example_data <- data.frame(
-    x = c(
-      stats::runif(10),
-      stats::runif(10)
-    ),
-    group = c("A", "B")
-  )
-  expect_equal(nrow(bootstrap_measures(
-    example_data,
-    bootstrap_iterations
-  )), bootstrap_iterations)
+test_that("Output is a data frame", {
+  num_groups <- 3
+
+  example_data <- group_by(data.frame(
+    measure = c(rep(
+      stats::runif(10), num_groups
+    )),
+    group = paste0("group_", seq(num_groups))
+  ), group)
+
+  expect_s3_class(bootstrap_measures(example_data,
+    kpi1 = mean(measure),
+    kpi2 = sum(measure)
+  ), "data.frame")
+})
+
+test_that("Output has bootstrap_iterations*num_groups rows", {
+  bootstrap_iterations <- 173
+  num_groups <- 7
+  example_data <- group_by(data.frame(
+    x = c(rep(
+      stats::runif(10), num_groups
+    )),
+    group = paste0("group_", seq(num_groups))
+  ), group)
+  expect_equal(nrow(
+    bootstrap_measures(example_data,
+      bootstrap_iterations,
+      kpi1 = mean(x)
+    )
+  ), bootstrap_iterations * num_groups)
 })
